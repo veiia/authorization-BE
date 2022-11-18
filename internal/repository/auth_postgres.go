6a8c32ab007c392c0ff14ca9b28ad9af93a44c71
syth0le <chdan565@gmail.com>
2022-11-18 16:46:39 +0300

    VEIIA-41 create endpoints for auth service: restructured routers and methods

--- internal/repository/auth_postgres.go
+++ internal/repository/auth_postgres.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	userModel "github.com/syth0le/authorization-BE/internal/domain/user"
+	"github.com/syth0le/authorization-BE/pkg/database"
 )
 
 type AuthPostgres struct {
@@ -16,7 +17,7 @@ func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 
 func (r *AuthPostgres) CreateUser(user userModel.User) (int, error) {
 	var id int
-	query := fmt.Sprintf("INSERT INTO %s (email, name, username, encrypted_password) values ($1, $2, $3, $4) RETURNING id")
+	query := fmt.Sprintf("INSERT INTO %s (name, username, password_hash) values ($1, $2, $3) RETURNING id", database.UsersTable)
 	row := r.db.QueryRow(query, user.Email, user.Name, user.Username, user.Password)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
