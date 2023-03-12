0a331f347d44370e57252a3bb6e0144f6377073c
syth0le <chdan565@gmail.com>
2023-03-13 01:26:46 +0300

    VEIIA-41 added structures

--- internal/domain/core/types.go
+++ internal/domain/core/types.go
@@ -1,9 +1,10 @@
 package coreModel
 
 type User struct {
-	Id       int    `json:"-"`
-	Name     string `json:"name" binding:"required"`
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required" db:"encrypted_password"`
-	Email    string `json:"email" binding:"required"`
+	Id        int    `json:"-"`
+	Name      string `json:"name" binding:"required"`
+	Username  string `json:"username" binding:"required"`
+	Password  string `json:"password" binding:"required" db:"encrypted_password"`
+	Email     string `json:"email" binding:"required"`
+	LastLogin string `json:"last_login" binding:"required"`
 }
--- internal/domain/jwt/types.go
+++ internal/domain/jwt/types.go
@@ -1,5 +1,9 @@
 package jwtModel
 
 type JWTToken struct {
-	Token string `json:"token"`
+	Id      int    `json:"-"`
+	Token   string `json:"token" binding:"required"`
+	Ttl     string `json:"ttl" binding:"required"`
+	IsAlive bool   `json:"is_alive" binding:"required" db:"is_alive"`
+	UserId  int    `json:"user_id" binding:"required" db:"user_id"`
 }
