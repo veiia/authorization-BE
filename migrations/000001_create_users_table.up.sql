6a291585ec24e66ea665518be0acd86a7c31c199
syth0le <chdan565@gmail.com>
2023-03-13 00:35:30 +0300

    VEIIA-41 added right sql schemes and migration files with scripts in Makefile

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS users (
+     id BIGSERIAL NOT NULL PRIMARY KEY,
+     email VARCHAR (255) NOT NULL UNIQUE,
+     name VARCHAR (255) NOT NULL,
+     username VARCHAR (255) NOT NULL UNIQUE,
+     encrypted_password VARCHAR NOT NULL,
+     last_login TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS tokens (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    token VARCHAR(255) NOT NULL UNIQUE,
+    ttl TIMESTAMP NOT NULL,
+    is_alive BOOLEAN NOT NULL DEFAULT FALSE,
+    user_id INT,
+    CONSTRAINT user_id
+        FOREIGN KEY(user_id)
+            REFERENCES users(id)
+)
