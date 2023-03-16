790c4a073b0b6106bc8b4d0043a608982d1883a8
syth0le <chdan565@gmail.com>
2023-03-16 23:45:22 +0300

    VEIIA-41: added dirty logic to all handlers

--- internal/handler/core/actions.go
+++ internal/handler/core/actions.go
@@ -15,33 +15,43 @@ func SignUp(c *gin.Context, h *handler.Handler) {
 	}
 	userId, err := h.Services.Authorization.CreateUser(request.(coreModel.User))
 	if err != nil {
-		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		utils.NewErrorResponse(c, http.StatusBadRequest, err.Error())
 	}
 	token, err := h.Services.Authorization.GenerateToken(request.Username, request.Password)
 	if err != nil {
-		return
+		utils.NewErrorResponse(c, http.StatusBadRequest, err.Error())
 	}
 	response := coreModel.SignUpResponse{Username: request.Username, JwtToken: token, IsAliveJwtToken: token.IsAlive}
 	c.JSON(http.StatusOK, response)
 }
 
 func SignIn(c *gin.Context, h *handler.Handler) {
-	var input coreModel.SignInRequest
-	if err := c.BindJSON(&input); err != nil {
+	var request coreModel.SignInRequest
+	if err := c.BindJSON(&request); err != nil {
 		utils.NewErrorResponse(c, http.StatusBadRequest, err.Error())
 	}
-	response, err := h.Services.Authorization.GenerateToken(input)
-	//if err != nil {
-	//	utils.NewErrorResponse(c, http.StatusBadRequest, "Wrong password or sign-in. Try again")
-	//}
-	//c.JSON(http.StatusOK, response) // TODO: return JWTToken
+	_, err := h.Services.Authorization.CheckSignIn(request)
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	response, err := h.Services.Authorization.GenerateToken(request.Username, request.Password) // remake to another params
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusBadRequest, "Wrong password or sign-in. Try again")
+	}
+	c.JSON(http.StatusOK, response)
 }
 
 func SignOut(c *gin.Context, h *handler.Handler) {
-	var input coreModel.SignOutRequest
-	if err := c.BindJSON(&input); err != nil {
+	var request coreModel.SignOutRequest
+	if err := c.BindJSON(&request); err != nil {
 		utils.NewErrorResponse(c, http.StatusBadRequest, err.Error())
 	}
+	response, err := h.Services.Authorization.ParseToken()
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+	c.JSON(http.StatusOK, response)
 }
 
 // for test
